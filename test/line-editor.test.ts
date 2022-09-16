a28bf8e33a75c961a53c4d4277d8adddcd8dda96
Wei Huang <whuang@arubanetworks.com>
2022-09-16 19:44:53 +0800

    case2 update anchor positions when line update

--- test/line-editor.test.ts
+++ test/line-editor.test.ts
@@ -11,7 +11,17 @@ describe('line editor', () => {
         expect(editor.findOne('.0-anchor').getAttrs()).toMatchObject({x: 10, y: 10})
         expect(editor.findOne('.1-anchor').getAttrs()).toMatchObject({x: 30, y: 30})
     });
-    //TODO update anchor when line update
+    it('should update anchor positions when line update', function () {
+        let line = new Konva.Line({points: [10, 10, 30, 30]})
+        let editor = new LineEditor()
+        editor.attach(line)
+
+        line.points([40, 40, 60, 60])
+
+        expect(editor.findOne('.0-anchor').getAttrs()).toMatchObject({x: 40, y: 40})
+        expect(editor.findOne('.1-anchor').getAttrs()).toMatchObject({x: 60, y: 60})
+    });
+
     //TODO update control point when line update
     //TODO change line points when dragging anchor
     //TODO add new anchor on editor when dragging control point
