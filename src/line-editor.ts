d792216f9acc092c243208c33e3030d06dd6687e
Wei Huang <whuang@arubanetworks.com>
2022-09-19 14:31:14 +0800

    refactor: create and update

--- src/line-editor.ts
+++ src/line-editor.ts
@@ -4,24 +4,9 @@ export class LineEditor extends Konva.Group {
     private line?: Konva.Line
 
     attach(line: Konva.Line) {
-        let points = line.points()
-        let pre = -1
-        for (let i = 0; i < points.length/2; i++) {
-            let curX = points[i*2]
-            let curY = points[i*2+1]
-            this.add(new Konva.Circle({ name:`${i}-anchor`, radius:10, x:curX, y:curY }))
-            if (pre !== -1) {
-                let preX = points[pre*2]
-                let preY = points[pre*2+1]
-
-                this.add(new Konva.Circle({ name:`${i}-control`, radius:10,
-                    x:preX+(curX-preX)/2, y:preY+(curY-preY)/2 }))
-            }
-            pre = i
-        }
-
         this.line = line
         line.on('pointsChange', () => {this.update()})
+        this.update()
     }
 
     update() {
@@ -30,15 +15,25 @@ export class LineEditor extends Konva.Group {
         for (let i = 0; i < points.length/2; i++) {
             let curX = points[i*2]
             let curY = points[i*2+1]
-            this.findOne(`.${i}-anchor`).setAttrs({x:curX, y:curY })
+            this.get(`${i}-anchor`).setAttrs({x:curX, y:curY })
             if (pre !== -1) {
                 let preX = points[pre*2]
                 let preY = points[pre*2+1]
 
-                this.findOne(`.${i}-control`).setAttrs(
+                this.get(`${i}-control`).setAttrs(
                     {x:preX+(curX-preX)/2, y:preY+(curY-preY)/2 })
             }
             pre = i
         }
+    }
+
+    private get(name: string) {
+        return this.findOne(`.${name}`) || this.create(name)
+    }
+
+    private create(name: string) {
+        let point = new Konva.Circle({name, radius: 10})
+        this.add(point)
+        return point
     }
 }
